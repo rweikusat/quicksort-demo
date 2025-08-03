167b141108150a071c804dfd567a0746ba2c09a8
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-03 12:32:42 +0100

    in theory, this should be complete

--- quicksort.c
+++ quicksort.c
@@ -25,6 +25,47 @@ static void print_nums(int *nums, unsigned n)
     putchar('\n');
 }
 
+static void swap(int *nums, unsigned p0, unsigned p1)
+{
+    int x;
+
+    x = nums[p0];
+    nums[p0] = nums[p1];
+    nums[p1] = x;
+}
+
+static void qs(int *nums, unsigned l, unsigned r)
+{
+    int pivot;
+    unsigned ol, or;
+
+    switch (r + 1 - l) {
+    case 2:
+        if (nums[l] > nums[r - 1]) swap(nums, l, r);
+
+    case 1:
+    case 0:
+        return;
+    }
+
+    pivot = nums[r];
+    ol = l;
+    or = r;
+    while (l < r) {
+        if (nums[l] > pivot) {
+            while (r > l && nums[r] > pivot) --r;
+
+            if (r > l) {
+                swap(nums, l, r);
+                ++l;
+            }
+        } else ++l;
+    }
+
+    qs(nums, ol, l - 1);
+    qs(nums, l, or);
+}
+
 /**  main */
 int main(int argc, char **argv)
 {
@@ -38,7 +79,8 @@ int main(int argc, char **argv)
     --argc;
     nums = alloca(argc * sizeof(int));
     fill_nums(argv + 1, nums);
-    print_nums(nums, argc);
+    qs(nums, 0, argc);
+    print_nums(nums, argc - 1);
 
     return 0;
 }
