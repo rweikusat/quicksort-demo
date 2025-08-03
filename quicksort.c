ebe4a5bbcbe253e6083648c597d4b28df67e0636
Rainer Weikusat <rweikusat@talktalk.net>
2025-08-03 12:18:21 +0100

    main program "without sorting"

--- quicksort.c
+++ quicksort.c
@@ -5,10 +5,22 @@
 /**  includes */
 #include <alloca.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**  main */
-int main(void)
+int main(int argc, char **argv)
 {
-    puts("Ha!");
+    int *nums;
+
+    if (argc < 2) {
+        fputs("Usage: quicksort <number>+\n", stderr);
+        exit(1);
+    }
+
+    --argc;
+    nums = alloca(argc * sizeof(int));
+    fill_nums(argv + 1, nums);
+    print_nums(nums, argc);
+
     return 0;
 }
